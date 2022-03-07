b5b75b780c8d8a5cdb6d3fd230bf6a579e4e6017
Przemurb <przemurb@hotmail.it>
2022-03-07 13:42:42 +0100

    GuestBook: - utworzenie klasy DAO i metody zwracającej listę wpisów

---
+++ src/main/java/com/example/guestbook/GuestBookDao.java
@@ -0,0 +1,42 @@
+package com.example.guestbook;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuestBookDao {
+    private final DataSource dataSource;
+
+    public GuestBookDao() {
+        try {
+            this.dataSource = DataProvider.getDataSource();
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    List<GuestBookEntry> allEntries() {
+        final String sql = "SELECT nick, content FROM entry";
+        List<GuestBookEntry> entriesList = new ArrayList<>();
+        try (
+                Connection connection = dataSource.getConnection();
+                Statement statement = connection.createStatement()
+              ){
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                String nick = resultSet.getString("nick");
+                String content = resultSet.getString("content");
+                entriesList.add(new GuestBookEntry(nick, content));
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return entriesList;
+    }
+}
